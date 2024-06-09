dfba07b6647e664f95a54888c13095db9162e714
Zander Lewis <wolfthedev@gmail.com>
2024-06-09 12:21:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,8 +23,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
